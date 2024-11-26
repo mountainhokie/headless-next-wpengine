70f255353353489a61c033384518ced9cfd8d56e
Justin <42555501+mountainhokie@users.noreply.github.com>
2024-11-26 16:40:04 -0500

    Basic GQL connection

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,65 @@
-export default function Home() {
-  return <>Headless Starter</>;
+import { client } from "@/lib/utils/apollo";
+import { gql } from "@apollo/client";
+
+interface AllPageProps {
+  pages: PagesProps;
+}
+
+interface PagesProps {
+  nodes: PageNodes;
+}
+
+interface PageNodes {
+  map(
+    arg0: (page: PageProps) => import("react").JSX.Element
+  ): import("react").ReactNode;
+  title: string;
+}
+
+interface PageProps {
+  title: string;
+}
+
+export default function Home({ pages }: AllPageProps) {
+  console.log(pages);
+  return (
+    <>
+      <main className="p-16">
+        <h1 className="text-4xl text-center font-bold">Headless Starter</h1>
+        {pages.nodes &&
+          pages.nodes.map((page) => {
+            console.log(page.title);
+            return (
+              <>
+                {page.title}
+                <br />
+              </>
+            );
+          })}
+      </main>
+    </>
+  );
+}
+
+export async function getStaticProps() {
+  const TEST_QUERY = gql`
+    query NewQuery {
+      pages {
+        nodes {
+          title
+        }
+      }
+    }
+  `;
+
+  const response = await client.query({
+    query: TEST_QUERY,
+  });
+
+  const pages = response?.data.pages;
+  return {
+    props: {
+      pages: pages,
+    },
+  };
 }
